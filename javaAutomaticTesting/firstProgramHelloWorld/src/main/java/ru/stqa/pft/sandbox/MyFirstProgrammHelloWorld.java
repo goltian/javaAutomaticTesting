3c03e58b5be04ce6a6c573448a53b936d28189f8
Лембриков Степан Андреевич <Stepan.Lembrikov@norbit.ru>
2026-03-11 11:19:12 +0300

    add fisrt function

--- javaAutomaticTesting/firstProgramHelloWorld/src/main/java/ru/stqa/pft/sandbox/MyFirstProgrammHelloWorld.java
+++ javaAutomaticTesting/firstProgramHelloWorld/src/main/java/ru/stqa/pft/sandbox/MyFirstProgrammHelloWorld.java
@@ -6,12 +6,16 @@ public class MyFirstProgrammHelloWorld {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        printSmth("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            printSmth("i = " + i);
         }
+    }
+
+    public static void printSmth(String smth) {
+        System.out.println(smth);
     }
 }
